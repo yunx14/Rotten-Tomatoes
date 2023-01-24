304bb595f0a52409ea39ebf1ea483c7807844609
charles johnston <charlesjohnston@charless-MacBook-Pro.local>
2023-01-23 23:23:03 -0600

    workflow for adding new reviews

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 var exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -9,12 +14,38 @@ app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
 
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II" },
-    { title: "Awesome Movie", movieTitle: "Titanic" }
-  ]
+const Review = mongoose.model('Review', {
+    title: String,
+    description: String,
+    movieTitle: String
+  });
+
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" }
+//   ]
   
-  // INDEX
-  app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+app.get('/', (req, res) => {
+    Review.find()
+      .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+
+  // NEW
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+  })
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+      console.log(review);
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err.message);
+    })
   })
