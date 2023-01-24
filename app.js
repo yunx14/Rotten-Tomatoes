8321731132eca264eaaa2d10b2a640e5175dd74d
charles johnston <charlesjohnston@charless-MacBook-Pro.local>
2023-01-23 22:51:45 -0600

    added a reviews page with sample reviews

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const app = express();
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-  })
-
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+  ]
+  
+  // INDEX
+  app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
+  })
